f8defae785cf54569ae3070294f05648fcfacac0
Mohammad Naushad Rain <rmohammad21@tbac.edu.np>
2025-06-12 10:42:45 +0545

    second commit

---
+++ zegocloudy/src/Room/index.jsx
@@ -0,0 +1,33 @@
+import React, { PureComponent } from 'react';
+import { useParams } from 'react-router-dom';
+import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
+function RoomPage() {
+    const { roomId } = useParams();
+    const myMeeting = async (element) => {
+        const appID = 1679227403;
+        const serverSecret = "0e6d42053805e326752e86fcfaf2bb12";
+        const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomId, Date.now().toString(), "Naushad Rain");
+        const zc = ZegoUIKitPrebuilt.create(kitToken);
+        zc.joinRoom({
+            container: element,
+            sharedLinks: [
+                {
+                    name: 'Copy Link',
+                    url: `http://localhost:3000/room/${roomId}`,
+                }
+            ],
+            scenario: {
+                mode: ZegoUIKitPrebuilt.OneONoneCall,
+
+            },
+            showScreenSharingButton: false,
+        })
+    }
+    return (
+        <div>
+            <div ref={myMeeting} />
+        </div>
+    );
+}
+
+export default RoomPage;
---
+++ zegocloudy/src/pages/Home/index.jsx
@@ -0,0 +1,17 @@
+import React, {useState,useCallback} from "react";
+import { useNavigate } from "react-router-dom";
+function HomePage() {
+    const [value,setValue] = useState();
+    const navigate = useNavigate();
+    const handleJoinRoom = useCallback(() =>{
+        navigate(`/room/${value}`);
+    },[navigate,value]);
+    return ( 
+        <div>
+            <input value={value} onChange={(e) => setValue(e.target.value)} type="text" placeholder="Enter Room Code" />
+            <button onClick={handleJoinRoom}>Join Now</button>
+        </div>
+     );
+}
+
+export default HomePage;
