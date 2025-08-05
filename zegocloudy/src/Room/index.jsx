a406aad7812c3710822ac4560f60c685fb0a5bba
Mohammad Naushad Rain <104451000+naushadrain@users.noreply.github.com>
2025-08-05 10:16:24 +0545

    Update index.jsx
    key change

--- zegocloudy/src/Room/index.jsx
+++ zegocloudy/src/Room/index.jsx
@@ -5,8 +5,10 @@ import './RoomPage.css';
 function RoomPage() {
     const { roomId } = useParams();
     const myMeeting = async (element) => {
-        const appID = 1679227403;
-        const serverSecret = "0e6d42053805e326752e86fcfaf2bb12";
+        //const appID = 1679227403;
+        const appID = 1649933003;
+        //const serverSecret = "0e6d42053805e326752e86fcfaf2bb12";
+        const serverSecret = "750238dd3e5fbcb411350d3a9f9ceb29"
         const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomId, Date.now().toString(), "Dr. Kumud");
         const zc = ZegoUIKitPrebuilt.create(kitToken);
         zc.joinRoom({
